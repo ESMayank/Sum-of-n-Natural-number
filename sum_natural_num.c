97881743aeeb030c2764fdd82f438f76e999ade5
ESMayank <114311263+ESMayank@users.noreply.github.com>
2022-11-20 07:38:56 +0530

    Add files via upload
    Code For Add natural number n times

---
+++ sum_natural_num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int sum,num;
+printf("which n natural num u want\n");
+scanf("%d",&num);
+sum=(num*num+num)/2;
+printf("the sum is %d",sum);
+
+
+
+
+    return 0;
+
+}
